d03e86b616a50fcd48f4f9475b030ab9aa8a770b
ninuush <76443832+ninuush@users.noreply.github.com>
2021-02-20 12:17:11 +0100

    built weather forecast

--- src/script.js
+++ src/script.js
@@ -28,6 +28,23 @@ let day = days[now.getDay()];
 
 time.innerHTML = `${day}, ${hours}:${minutes}`;
 
+function formatHours(timestamp) {
+let now = new Date(timestamp);
+let hours = now.getHours();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = now.getMinutes();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+
+  return `${hours}:${minutes}`;
+}
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#location");
@@ -67,10 +84,34 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  
+  for (let index = 0; index < 4; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+                <div class="col">
+                    <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+                    alt=""
+                    />
+                    <br />
+                    ${formatHours(forecast.dt * 1000)}
+                    <br />
+                    ${Math.round(forecast.main.temp_max)}° | ${Math.round(forecast.main.temp_min)}°
+                </div>`;
+  }
+
+}
+
 function search(city) {
   let apiKey = "8bc187cbfbf86ab7d6f8399ce1d178ae";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
